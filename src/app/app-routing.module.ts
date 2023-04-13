fc0744cd76d2e6754b56bc58bac5ddaee2d1942c
KareemE125 <kareemezzat.125@gmail.com>
2023-04-13 15:11:34 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChildComponent } from './child/child.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'child', component: ChildComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'child', component: ChildComponent },
+  { path: 'login', component: LoginComponent },
 
 ];
 
