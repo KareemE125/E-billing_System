27540d49a658f0b4f2479f8fdf1b087919465a67
Salah Ashraf <salahashraf924@gmail.com>
2023-04-17 14:07:32 +0200

    add about-us component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ParentComponent } from './parent/parent.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     LoginComponent,
     HomeComponent,
     ParentComponent,
-    SignUpComponent
+    SignUpComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
