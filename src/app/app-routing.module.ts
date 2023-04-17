6961364d7ab9258284139d398275179fe7a576d2
Salah Ashraf <salahashraf924@gmail.com>
2023-04-17 14:08:10 +0200

    update route for about-us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ChildComponent } from './child/child.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'child', component: ChildComponent },
   { path: 'login', component: LoginComponent },
+  {path:'about-us',component:AboutUsComponent}
 
 ];
 
