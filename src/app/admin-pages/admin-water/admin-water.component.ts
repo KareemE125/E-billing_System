8fae9e089701f02c3a1ee031dae3ce5c62df9189
KareemE125 <kareemezzat.125@gmail.com>
2023-04-18 23:09:33 +0200

    Admin Electricity

--- src/app/admin-pages/admin-water/admin-water.component.ts
+++ src/app/admin-pages/admin-water/admin-water.component.ts
@@ -10,20 +10,6 @@ export class AdminWaterComponent {
 
   tableUnit: string = "m3";
 
-  infoList: CommonBill[] = [
-    {
-      id:'1', year: 2021, month: 9 , total: 500, isPaid: false, penalty: 20, usage: 890
-    },
-    {
-      id:'1',year: 2022, month: 1 , total: 800, isPaid: true, penalty: 0, usage: 0,
-    },
-    {
-      id:'1',year: 2021, month: 6 , total: 1200, isPaid: false, penalty: 10, usage: 555,
-    },
-    {
-      id:'1',year: 2023, month: 2 , total: 1500, isPaid: true, penalty: 30, usage: 230,
-    }
-  ];
   userList: User[] = [
     { id: '1', name: 'John Doe', year: 2021, month: 9 , total: 500, isPaid: false, penalty: 20, usage: 890 },
     { id: '2', name: 'Jane Doe', year: 2022, month: 1 , total: 800, isPaid: true, penalty: 0, usage: 0 },
