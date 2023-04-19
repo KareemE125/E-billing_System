a537ed4a0aa4b9d1bef394d913779d5fbfa2cb33
KareemE125 <kareemezzat.125@gmail.com>
2023-04-20 01:42:05 +0200

    Make Admin tables reusable

---
+++ src/app/admin-pages/admin-common-table/admin-common-table.component.ts
@@ -0,0 +1,92 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-common-table',
+  templateUrl: './admin-common-table.component.html',
+  styleUrls: ['./admin-common-table.component.css']
+})
+export class AdminCommonTableComponent {
+
+  @Input() type: number = 1;
+  @Input() tableUnit: string = "kWh";
+  @Input() userList: any[] = [];
+  
+
+  tableInternetUnit: string = "MB";
+  tableMinutesUnit: string = "min";
+
+
+  filteredUserList: User[] = [];
+
+  searchText = '';
+  selectedOption = 'Choose an option';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // set initial filtered list to full list
+    this.filteredUserList = [...this.userList];
+  }
+
+  filterUsers(): void {
+    // reset filtered list to full list
+    this.filteredUserList = [...this.userList];
+
+    // apply search filter
+    if (this.searchText) {
+      this.selectedOption = 'Choose an option'
+      const searchTextLower = this.searchText.toLowerCase();
+      this.filteredUserList = this.filteredUserList.filter(user => {
+        return user.name.toLowerCase().includes(searchTextLower)
+          || user.id.toLowerCase().includes(searchTextLower)
+          || user.year.toString().includes(searchTextLower)
+          || user.month.toString().includes(searchTextLower)
+          || user?.spOffer?.toLowerCase().toString().includes(searchTextLower);
+      });
+    }
+
+    // apply combobox filter
+    if (this.selectedOption) {
+      switch (this.selectedOption) {
+        case 'paid':
+          this.filteredUserList = this.filteredUserList.filter(info => {
+            return info.isPaid == true;
+          });
+          break;
+        case 'unpaid':
+          this.filteredUserList = this.filteredUserList.filter(info => {
+            return info.isPaid == false;
+          });
+          break;
+        case 'penalty':
+          this.filteredUserList = this.filteredUserList.filter(info => {
+            return info.penalty > 0;
+          });
+          break;
+        case 'notPenalty':
+          this.filteredUserList = this.filteredUserList.filter(info => {
+            return info.penalty < 1;
+          });
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+
+}
+
+export interface User {
+  spOffer: any;
+  internet: any;
+  minutes: any;
+  id: string;
+  name: string;
+  year: number,       
+  month: number,      
+  usage: any,      
+  penalty: number,    
+  total: number,     
+  isPaid: boolean, 
+}
--- src/app/admin-pages/admin-electricity/admin-electricity.component.ts
+++ src/app/admin-pages/admin-electricity/admin-electricity.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonBill } from 'src/app/models/bills/commonBill.model';
+import { User } from '../admin-common-table/admin-common-table.component';
 
 @Component({
   selector: 'app-admin-electricity',
@@ -8,10 +9,10 @@ import { CommonBill } from 'src/app/models/bills/commonBill.model';
 })
 export class AdminElectricityComponent {
 
-
+  type: number = 1;
   tableUnit: string = "kWh";
-
-  userList: User[] = [
+  
+  userList: any[] = [
     { id: '1', name: 'John Doe', year: 2021, month: 9 , total: 500, isPaid: false, penalty: 20, usage: 890 },
     { id: '2', name: 'Jane Doe', year: 2022, month: 1 , total: 800, isPaid: true, penalty: 0, usage: 0 },
     { id: '3', name: 'zzz', year: 2021, month: 6 , total: 1200, isPaid: false, penalty: 10, usage: 555 },
@@ -20,72 +21,5 @@ export class AdminElectricityComponent {
     // add more users as needed
   ];
 
-  filteredUserList: User[] = [];
-
-  searchText = '';
-  selectedOption = 'Choose an option';
-
-  constructor() { }
-
-  ngOnInit(): void {
-    // set initial filtered list to full list
-    this.filteredUserList = [...this.userList];
-  }
-
-  filterUsers(): void {
-    // reset filtered list to full list
-    this.filteredUserList = [...this.userList];
-
-    // apply search filter
-    if (this.searchText) {
-      this.selectedOption = 'Choose an option'
-      const searchTextLower = this.searchText.toLowerCase();
-      this.filteredUserList = this.filteredUserList.filter(user => {
-        return user.name.toLowerCase().includes(searchTextLower)
-            || user.id.toLowerCase().includes(searchTextLower)
-            || user.year.toString().includes(searchTextLower) 
-            || user.month.toString().includes(searchTextLower);
-      });
-    }
-
-    // apply combobox filter
-    if (this.selectedOption) {
-      switch (this.selectedOption) {
-        case 'paid':
-          this.filteredUserList = this.filteredUserList.filter(info => {
-            return info.isPaid == true;
-          });
-          break;
-        case 'unpaid':
-          this.filteredUserList = this.filteredUserList.filter(info => {
-            return info.isPaid == false;
-          });
-          break;
-        case 'penalty':
-          this.filteredUserList = this.filteredUserList.filter(info => {
-            return info.penalty > 0;
-          });
-          break;
-        case 'notPenalty':
-          this.filteredUserList = this.filteredUserList.filter(info => {
-            return info.penalty < 1;
-          });
-          break;
-        default:
-          break;
-      }
-    }
-  }
-
-}
-
-interface User {
-  id: string;
-  name: string;
-  year: number,       //1990 <= year <= 2022
-  month: number,      //1 <= month <= 12
-  usage: number,      //0 <= usage <= 100000
-  penalty: number,    //0 <= penalty <= 100000
-  total: number,      //0 <= total <= 100000
-  isPaid: boolean,   //true or false
+  
 }
