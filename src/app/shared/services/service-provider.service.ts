5890305b7e10530ce2419c69813875aaa0e4071f
Salah Ashraf <salahashraf924@gmail.com>
2023-04-22 18:49:02 +0200

    implement get all services providers

--- src/app/shared/services/service-provider.service.ts
+++ src/app/shared/services/service-provider.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ServiceProvider } from '../../models/users/serviceProvider.model';
-
+import { DataService } from './BillService.service';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore'
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceProviderService {
 
-  constructor() { }
+  serviceProvidersCollection: AngularFirestoreCollection<ServiceProvider>
+
+  constructor(private db: AngularFirestore) {
+    this.serviceProvidersCollection = this.db.collection("/service-providers"); 
+      
+  }
 
   //Todo: add user to firebase
   async addServiceProvider(sp: ServiceProvider): Promise<ServiceProvider | null> {
@@ -22,7 +28,7 @@ export class ServiceProviderService {
 * @param password
 * @returns user if found, false if not found, null otherwise
 */
-  async validateServiceProvider(email: string, password: string): Promise<ServiceProvider | null | false> {
+  async validateServiceProvider(email: string, password: string) {
     // try {
     //   const user = await this.getUserByEmail(email);
     //   if (user != null && user != false && user.password === password) {
@@ -36,35 +42,33 @@ export class ServiceProviderService {
     //   console.log('Error getting user:', error);
     //   return null;
     // }
-    return false;
+    // return false;
   }
 
-  /**
- * 
- * @returns all users if found, null otherwise
+/** 
+ * @returns all serviceProviders if found, null otherwise
  */
   async getAllServiceProviders(): Promise<ServiceProvider[] | null> {
-    // console.log('Getting all users...');
-    // try {
-    //   const querySnapshot = await this.userCollection.get().toPromise();
-    //   const users: User[] = [];
+    console.log('Getting all services providers...');
+    try {
+      const querySnapshot = await this.serviceProvidersCollection.get().toPromise();
+      const serviceProviders: ServiceProvider[] = [];
 
-    //   if (querySnapshot !== undefined) {
-    //     querySnapshot.forEach((doc) => {
-    //       if (doc.exists) {
-    //         // console.log('User data:', doc.data());
-    //         users.push(doc.data() as User);
-    //       } else {
-    //         console.error('No such document!');
-    //       }
-    //     });
-    //   }
-    //   return users;
-    // } catch (error) {
-    //   console.error('Error getting users:', error);
-    //   return null;
-    // }
-    return null
+      if (querySnapshot !== undefined) {
+        querySnapshot.forEach((doc) => {
+          if (doc.exists) {
+            // console.log('User data:', doc.data());
+            serviceProviders.push(doc.data() as ServiceProvider);
+          } else {
+            console.error('No such document!');
+          }
+        });
+      }
+      return serviceProviders;
+    } catch (error) {
+      console.error('Error getting services providers:', error);
+      return null;
+    }
   }
 
 }
