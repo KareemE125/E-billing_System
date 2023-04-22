d28fdad5ed400b5c7d4507c7e44968a5571216f3
Salah Ashraf <salahashraf924@gmail.com>
2023-04-22 23:02:13 +0200

    finish admin service

--- src/app/shared/services/admin.service.ts
+++ src/app/shared/services/admin.service.ts
@@ -1,41 +1,73 @@
 import { Injectable } from '@angular/core';
 import { Admin } from '../../models/users/admin.model';
+import { DataService } from './BillService.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore'
 @Injectable({
   providedIn: 'root'
 })
-export class AdminService {
+export class AdminService extends DataService {
 
-  constructor() { }
-
-  //Todo: add admin to firebase
+  constructor(db: AngularFirestore ) {
+    super(db);
+  }
   async addAdmin(admin: Admin): Promise<Admin | null> {
-    console.log(`Adding admin ${JSON.stringify(admin)} to firebase`)
-    return null;
+    const newDocRef = this.adminCollection.doc();
+    admin.id = newDocRef.ref.id;
+    try {
+      await newDocRef.set({ ...admin });
+      console.log('Admin added to firebase: ', admin)
+      return admin;
+    } catch (error) {
+      console.error('Error adding admin to firebase: ', error);
+      return null;
+    }
   }
 
-  updateAdmin(admin: Admin) {
-    console.log(`Updating admin ${JSON.stringify(admin)} to firebase`)
+  async updateAdmin(admin: Admin) : Promise<null | true>{
+    try{
+      console.log(`Updating admin ${JSON.stringify(admin)} to firebase`)
+      await this.adminCollection.doc(admin.id).update(admin);
+      return true;
+    }
+    catch(error){
+      console.error('Error updating admin: ', error);
+      return null;
+    }
   }
 
   /**
  * @param email
  * @param password
- * @returns user if found, false if not found, null otherwise
+ * @returns Admin if found, false if not found, null otherwise
  */
   async validateAdmin(email: string, password: string): Promise<Admin | null | false> {
-    // try {
-    //   const user = await this.getUserByEmail(email);
-    //   if (user != null && user != false && user.password === password) {
-    //     console.log("user is found and password is correct");
-    //     this.accountService.SetCurrentUser(user, UserType.User);
-    //     return user as User;
-    //   }
-    //   return false;
-    // }
-    // catch (error) {
-    //   console.log('Error getting user:', error);
-    //   return null;
-    // }
-    return false;
+    try {
+      const admin = await this.getAdminByEmail(email);
+      if (admin != null && admin != false && admin.password === password) {
+        console.log("user is found and password is correct");
+        return admin as Admin;
+      }
+      return false;
+    }
+    catch (error) {
+      console.log('Error getting user:', error);
+      return null;
+    }
+  }
+
+  async getAdminByEmail(email: string): Promise<Admin | null | false> {
+    console.log('Getting admin by email: ', email);
+    try {
+      const querySnapshot = await this.adminCollection.ref.where('email', '==', email).get();
+      if (!querySnapshot.empty) {
+        const adminDoc = querySnapshot.docs[0];
+        console.log("User is found with email: ", email)
+        return adminDoc.data() as Admin;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return null;
+    }
   }
 }
