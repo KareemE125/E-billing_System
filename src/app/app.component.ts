8359605d9cdf0a1753b5abc6f2b244dd4a06805a
Salah Ashraf <salahashraf924@gmail.com>
2023-04-24 00:17:01 +0200

    add dummy admins to the database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { DbService } from './shared/services/db.service';
 import { ToastService } from './shared/services/toast.service';
 import { Router } from '@angular/router';
 import { ServiceProvider } from './models/users/serviceProvider.model';
-import { getDummyServiceProviders, getDummyUsers } from './dummy-data/seed-database';
+import { getDummyServiceProviders, getDummyUsers, getDummyAdmins } from './dummy-data/seed-database';
 import { ServiceProviderService } from './shared/services/service-provider.service';
 import { AdminService } from './shared/services/admin.service';
 import { UserService } from './shared/services/user.service';
+import { Admin } from './models/users/admin.model';
 
 @Component({
   selector: 'app-root',
@@ -34,7 +35,10 @@ export class AppComponent implements OnInit {
     const users: User[] = getDummyUsers()
     for (let u of users) await this.userService.addUser(u);
 
+    const admins : Admin[]=getDummyAdmins();
+    for (let a of admins) await this.adminService.addAdmin(a);
   }
+
 
 
 
